6554ad261edc3a1308aa8378d86ab340d4feb795
Erkam USLU <ercome1994gmail.com>
2022-04-18 14:28:35 +0300

    18.04.2022

--- src/arrayContains/arrayContains.java
+++ src/arrayContains/arrayContains.java
@@ -10,23 +10,21 @@ public class arrayContains {
     Artan düzende sıralanmış iki adet ınt array göz önüne alındığında 2. deki tum degerler 1.de varsa true dondur
     yoksa false dondur.
      */
-
         int[] arr1 = {2, 4, 6, 8};
-        int[] arr2 = {2, 6};
+        int[] arr2 = {4, 6, 9};
+        int sayac = 0;
 
-        boolean varMi = false;
-
-        for (int i = 0; i < arr2.length; i++) {
-            for (int j = 0; j <=i ; j++) {
-
-                if (arr1[i]==arr2[j]){
-                    varMi=true;
-                }else varMi=false;
-
+        for (int each : arr1) {
+            for (int each1 : arr2) {
+                if (each == each1) {
+                    sayac++;
+                }
             }
         }
-        System.out.println(varMi);
-
-
-
-    }}
+        if (sayac == arr2.length) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
